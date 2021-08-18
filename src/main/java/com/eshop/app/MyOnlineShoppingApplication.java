4187531381c0235a2d5dec263c21465520db885e
Aravindkumar-Palani <palaniaravindkumar@gmail.com>
2021-08-18 14:41:09 +0530

    My initial commit

---
+++ src/main/java/com/eshop/app/MyOnlineShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOnlineShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOnlineShoppingApplication.class, args);
+	}
+
+}
