f9a8badfea1b0c73d0eb660a0026878fbd6fce37
Aravindkumar-Palani <palaniaravindkumar@gmail.com>
2021-08-19 08:22:59 +0530

    adding cross origin - session 5

--- src/main/java/com/eshop/controller/DataController.java
+++ src/main/java/com/eshop/controller/DataController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.eshop.model.ShoppingRequest;
 
@@ -38,7 +39,8 @@ public class DataController {
 			return "Please login!";
 		}
 	}
-	
+
+	@CrossOrigin(origins = "*")
 	@PostMapping(path="userLogin")
 	public String userAuthenticationViaPost(@RequestBody ShoppingRequest request) {
 		return loginService.authenticateUser(request);
