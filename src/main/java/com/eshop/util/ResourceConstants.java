9db0f24c06cd0e9e0f442a74fe46a5a8a8a92a21
Aravindkumar-Palani <palaniaravindkumar@gmail.com>
2021-09-19 14:07:59 +0530

    updating initial load file location

--- src/main/java/com/eshop/util/ResourceConstants.java
+++ src/main/java/com/eshop/util/ResourceConstants.java
@@ -2,8 +2,8 @@ package com.eshop.util;
 
 public class ResourceConstants {
 
-    public static final String categoryListLocation = "data-loader/categoryList.csv";
-    public static final String itemListLocation = "data-loader/itemList.xlsx";
-    public static final String carouselListLocation = "data-loader/homepage.csv";
+    public static final String categoryListLocation = "categoryList.csv";
+    public static final String itemListLocation = "itemList.xlsx";
+    public static final String carouselListLocation = "homepage.csv";
 
 }
